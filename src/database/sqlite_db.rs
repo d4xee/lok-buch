9bf41dd8deea5b6cd38fbbfaef4889df41bec7f1
d4xee <daxeckertobias@gmail.com>
2024-12-12 20:25:10 +0100

    fixed migrations bug in database.rs removed init method from database LokResourceManager works, but is only half driver-agnostic anymore added add method to LRM added test folder for testing utils

--- src/database/sqlite_db.rs
+++ src/database/sqlite_db.rs
@@ -2,9 +2,33 @@ use crate::database::{Database, DatabaseError};
 use sqlx::migrate::MigrateDatabase;
 use sqlx::{Pool, Sqlite, SqlitePool};
 
+/// SQLite implementation for the database.
 pub struct SQLiteDB {
     connection: Pool<Sqlite>,
     db_url: String,
+}
+
+impl SQLiteDB {
+    async fn migrate(connection: Pool<Sqlite>) -> Result<(), DatabaseError> {
+        // sqlite migrations
+        let crate_dir = std::env::current_dir().unwrap();
+        let migrations = std::path::Path::new(&crate_dir).join("./migrations");
+
+        let migration_results = sqlx::migrate::Migrator::new(migrations)
+            .await
+            .unwrap()
+            .run(&connection)
+            .await;
+
+        match migration_results {
+            Ok(_) => {
+                Ok(())
+            }
+            Err(err) => {
+                Err(DatabaseError::SpecificError(format!("Error running migrations: {}", err)))
+            }
+        }
+    }
 }
 
 impl Database for SQLiteDB {
@@ -15,10 +39,19 @@ impl Database for SQLiteDB {
             println!("Creating database {}", db_url);
             match Sqlite::create_database(db_url).await {
                 Ok(_) => {
-                    Ok(SQLiteDB {
-                        connection: Pool::connect_lazy(db_url).unwrap(),
-                        db_url: db_url.to_owned(),
-                    })
+                    let connection = Pool::connect(db_url).await;
+
+                    if connection.is_ok() {
+                        let connection = connection.unwrap();
+                        SQLiteDB::migrate(connection.clone()).await?;
+
+                        Ok(SQLiteDB {
+                            connection: connection.clone(),
+                            db_url: db_url.to_owned(),
+                        })
+                    } else {
+                        Err(DatabaseError::ConnectionError)
+                    }
                 }
                 Err(err) => {
                     Err(DatabaseError::GeneralError(format!("Failed to create SQLite database: {}", err)))
@@ -46,71 +79,31 @@ impl Database for SQLiteDB {
             }
         }
     }
-
-    async fn init(&self) -> Result<(), DatabaseError> {
-        // sqlite migrations
-        let crate_dir = std::env::current_dir().unwrap();
-        let migrations = std::path::Path::new(&crate_dir).join("./migrations");
-
-        let migration_results = sqlx::migrate::Migrator::new(migrations)
-            .await
-            .unwrap()
-            .run(&self.connection)
-            .await;
-
-        match migration_results {
-            Ok(_) => {
-                Ok(())
-            }
-            Err(err) => {
-                Err(DatabaseError::SpecificError(format!("Error running migrations: {}", err.to_string())))
-            }
-        }
-    }
 }
 
 #[cfg(test)]
 mod sqlite_db_tests {
     use super::*;
+    use crate::test;
     use async_std::task;
+    use std::time::Duration;
 
-    fn remove_test_db() {
-        match std::fs::remove_file("test/test.db") {
-            Ok(_) => { println!("Removed test db"); }
-            Err(_) => { println!("Failed to remove test db"); }
-        }
-    }
-
-    fn build_db() -> SQLiteDB {
-        remove_test_db();
-        task::block_on(SQLiteDB::build("sqlite://test/test.db")).unwrap()
-    }
 
     #[test]
     fn build_db_test() {
-        remove_test_db();
-        let result = task::block_on(SQLiteDB::build("sqlite://test/test.db"));
+        test::util::remove_test_db(0);
+        std::thread::sleep(Duration::from_millis(test::util::AFTER_DELETE_WAIT_TIME));
+        let result = task::block_on(SQLiteDB::build("sqlite://test/test0.db"));
 
         assert!(result.is_ok());
     }
 
     #[test]
     fn connect_to_db_test() {
-        let mut db = build_db();
+        let mut db = test::util::build_db(1);
 
         let result = task::block_on(db.connect());
 
         assert!(result.is_ok())
-    }
-
-    #[test]
-    fn init_db_test() {
-        let mut db = build_db();
-
-        let _ = task::block_on(db.connect());
-
-        let result = task::block_on(db.init());
-
-        assert!(result.is_ok());
     }
 }
