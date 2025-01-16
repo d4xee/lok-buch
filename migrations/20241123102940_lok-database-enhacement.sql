b0f0cf6dab0220ae2e8188c1c9b6af1f9d9b38fa
d4xee <daxeckertobias@gmail.com>
2025-01-16 19:42:10 +0100

    streamlined add and edit pages and functions and extracted common functionality to new methods

--- migrations/20241123102940_lok-database-enhacement.sql
+++ migrations/20241123102940_lok-database-enhacement.sql
@@ -1,3 +1,6 @@
-alter table loks add lokmaus_name varchar(10) not null default '';
-alter table loks add producer varchar(250) not null default '';
-alter table loks add management varchar(250) not null default '';
+alter table loks
+    add lokmaus_name varchar(10) default null;
+alter table loks
+    add producer varchar(250) default null;
+alter table loks
+    add management varchar(250) default null;
